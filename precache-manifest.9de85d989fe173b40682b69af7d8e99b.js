9627cdfe246d58548206ca25f9e0eb223c0c5b53
s13bp <86206902+s13bp@users.noreply.github.com>
2023-02-25 22:42:54 +0300

    tired much

---
+++ precache-manifest.9de85d989fe173b40682b69af7d8e99b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdeabf3fab04b3d8a81e65d79fdab4a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd826892abb06d2c03e7",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd826892abb06d2c03e7",
+    "url": "/static/js/main.43ba8531.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
